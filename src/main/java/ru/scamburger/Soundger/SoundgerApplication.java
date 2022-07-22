95de02ae1d92c8cd8f3cf187addc59f13c070ebc
Listanuv <solonovi9@gmail.com>
2022-07-23 02:17:43 +0300

    Create project

---
+++ src/main/java/ru/scamburger/Soundger/SoundgerApplication.java
@@ -0,0 +1,13 @@
+package ru.scamburger.Soundger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/scamburger/Soundger/SoundgerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.scamburger.Soundger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
