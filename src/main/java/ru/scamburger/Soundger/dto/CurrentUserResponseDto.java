e9d524a507f00615f1b92bf70164d1c49914a30d
Listanuv <solonovi9@gmail.com>
2022-07-27 12:29:24 +0300

    All fields on dto set private;

--- src/main/java/ru/scamburger/Soundger/dto/CurrentUserResponseDto.java
+++ src/main/java/ru/scamburger/Soundger/dto/CurrentUserResponseDto.java
@@ -1,9 +1,16 @@
 package ru.scamburger.Soundger.dto;
 
+import lombok.Data;
+
+@Data
 public class CurrentUserResponseDto {
-    
-    public Long id;
 
-    public String username;
+    private Long id;
 
+    private String username;
+
+    public CurrentUserResponseDto(Long id, String username) {
+        this.id = id;
+        this.username = username;
+    }
 }
